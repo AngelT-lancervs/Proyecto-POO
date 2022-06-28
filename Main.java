522146725b81b8b06f8eb697e9583981c9b85edb
JeryRPG <107366493+JeryRPG@users.noreply.github.com>
2022-06-27 22:36:52 -0500

    Commit inicial, creando clase principal

---
+++ Main.java
@@ -0,0 +1,10 @@
+package menu;
+
+public class Main {
+
+	public static void main(String[] args) {
+	String interfaz	= "°=-----==[Menú]==-----=°\n1.Servicios\n2.Empleados\n3.Clientes\n4.Citas\n5.Atenciones\n6.Salir\n°=-----==[*]==-----=°";
+    System.out.print(interfaz);
+    }
+
+}
