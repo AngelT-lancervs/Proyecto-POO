a4bf47f236e4eec4f180ee0d76220202d608710e
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 01:08:50 -0500

    Creación de constructor de la clase Atención
    falta buscar de donde sale el servicio

--- modelo/Atencion.java
+++ modelo/Atencion.java
@@ -3,4 +3,13 @@ package modelo;
 public class Atencion{
 	private Time duracionReal;
 	private Empleado empleado;
+	//falta servicio, quizás importarlo desde citas con un metodo
+	
+	public Atencion(Time duracionReal, Servicio servicio, Empleado empleado)
+	{
+		this.duracionReal = duracionReal;
+		this.servicio = servicio;
+		this.empleado = empleado;
+	}
+		
 }
