655003786ece3c40699c21a170a103f062d04bfb
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 01:34:45 -0500

    Creación de constructor de la clase Atención
    Asignar valores del arraylist

--- modelo/Servicio.java
+++ modelo/Servicio.java
@@ -5,4 +5,13 @@ public class Servicio{
     private String nombreServicio;
     private String duracion;
     private Empleado empleado;
+    
+    //crear un método para los servicios y agregarlo al constructor
+    
+    public Servicio(String nombreServicio, String duracion, Empleado empleado)
+    {
+        this.nombreServicio = nombreServicio;
+        this.duracion = duracion;
+        this.empleado = empleado;
+    }
 }
