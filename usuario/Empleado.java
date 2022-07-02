c5c7b05c263c987597dc64c07824fe701c3654e2
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 02:09:41 -0500

    Creación de constructor de la clase Empleado

--- usuario/Empleado.java
+++ usuario/Empleado.java
@@ -2,4 +2,13 @@ package usuario;
 
 public class Empleado extends Usuario{
     private boolean estado;
+    
+    //constructor de la clase
+    public Empleado(String nombre, String cedula, String email, boolean estado, String telefono)
+    {
+        super(nombre, cedula, email, estado, telefono);
+        this.estado = estado;
+    }
+    
+    
 }
