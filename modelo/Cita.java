dce06a50c8295713937f6e3c5d128cba239f4eca
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 00:38:08 -0500

    Update Cita.java
    creación del constructor de cita

--- modelo/Cita.java
+++ modelo/Cita.java
@@ -2,10 +2,21 @@ package modelo;
 
 public class Cita{
 	private Cliente cliente;
-	private Empleado empleado;
+	private Empleado proovedor;
 	private Servicio servicio;
 	private Time hora;
 	private Date fecha;
+	
+	public Cita(Date fecha, Time hora, Servicio servicio, Cliente cliente, Empleado proovedor)
+	{
+		this.cliente = cliente;
+		this.proovedor = proovedor;
+		this.servicio = servicio;
+		this.hora = hora;
+		this.fecha = fecha;
+		
+		
+	}
 
 
 }
