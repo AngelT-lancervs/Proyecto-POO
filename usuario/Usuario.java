b0fab8f8cc511cbf1866f32afe208fb5a86a64fe
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 02:20:59 -0500

    Creación de constructor de la clase padre Usuario

--- usuario/Usuario.java
+++ usuario/Usuario.java
@@ -5,5 +5,13 @@ public abstract class Usuario {
 	protected String cedula;
 	protected String telefono;
 	protected String email;
-
+	
+	//constructor de la clase Usuario
+	public Usuario(String nombre, String cedula, String telefono, String email)
+	{
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.telefono = telefono;
+		this.email = email;
+	}
 }
