0089beea08f539737840239450a17467af5c618e
AngelTomalaxd <108084684+AngelTomalaxd@users.noreply.github.com>
2022-07-02 01:44:18 -0500

    Creación de constructores de la clase Cliente

--- usuario/Cliente.java
+++ usuario/Cliente.java
@@ -2,4 +2,10 @@ package usuario;
 
 public class Cliente extends usuario {
 	private String datos_del_representante;
+	
+	public Cliente(String nombre, String email, String cedula, String dts_re, String telefono)
+	{
+		super(nombre, cedula, telefono, email);
+		this.datos_del_representante = dts_re;
+	}
 }
