6943287d0032b0a820fafebe4274bfa8b6fd4c01
AngelTomalaxd <aatomala@espol.edu.ec>
2022-08-06 10:42:34 -0500

    Agregar las descripciones previas a presionar los botones del menú

--- src/main/java/g05/MenuController.java
+++ src/main/java/g05/MenuController.java
@@ -3,6 +3,7 @@ package g05;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -29,39 +30,42 @@ public class MenuController {
     @FXML
     VBox vBoxMenu;
 
-
-
-
     //Métodos cuando el cursor pasa por encima de cada boton
     @FXML
     public void preServicios(){
         Image im = new Image(getClass().getResourceAsStream("media/servicio.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Podrá ver, agregar, editar y/o desactivar \n algún servicio. Presione el botón\n -Servicios- para continuar");
     }
     @FXML
     public void preClientes(){
         Image im = new Image(getClass().getResourceAsStream("media/cliente.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Podrá ver, agregar y/o editar clientes.\n Presione el botón -Clientes- para\n continuar");
     }
     @FXML
     public void preAtenciones(){
         Image im = new Image(getClass().getResourceAsStream("media/atencion.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Podrá ver, registrar y/o consultar las atenciones.\n Presione el botón -Atenciones- para\n continuar");
     }
     @FXML
     public void preCitas(){
         Image im = new Image(getClass().getResourceAsStream("media/cita.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Podrá ver, crear, eliminar, y/o consultar\n citas. Presione el botón -Citas- para\n continuar");
     }
     @FXML
     public void preEmpleados(){
         Image im = new Image(getClass().getResourceAsStream("media/empleado.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Podrá ver, agregar, editar y/o eliminar\n empleados. Presione el botón\n -Empleados- para continuar");
     }
     @FXML
     public void preSalir(){
         Image im = new Image(getClass().getResourceAsStream("media/salir.jpg"));
         displayMenu.setImage(im);
+        infoMenu.setText("Cerrará la sesión actual.\n Presione el botón -Salir- para cerrar\n el sistema");
     }
 
     //Métodos cuando se da click a cada boton
