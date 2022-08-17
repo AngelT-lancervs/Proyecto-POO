5d881700d45eb2ac5a0886dfdba28f8a95a2626e
AngelTomalaxd <aatomala@espol.edu.ec>
2022-08-17 00:51:24 -0500

    Avance en la actividad recreativa

--- src/main/java/g05/controlador/JuegoController.java
+++ src/main/java/g05/controlador/JuegoController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import g05.App;
@@ -95,10 +96,37 @@ public class JuegoController implements Initializable {
     @FXML
     private Label errores;
 
+    ArrayList<Integer> nms = JuegoController.generarNumeros();
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        this.label0_0.setText(String.valueOf(nms.get(0)));
+        this.label0_1.setText(String.valueOf(nms.get(1)));
+        this.label0_2.setText(String.valueOf(nms.get(2)));
+        this.label0_3.setText(String.valueOf(nms.get(3)));
+        this.label1_0.setText(String.valueOf(nms.get(4)));
+        this.label1_1.setText(String.valueOf(nms.get(5)));
+        this.label1_2.setText(String.valueOf(nms.get(6)));
+        this.label1_3.setText(String.valueOf(nms.get(7)));
+        this.label2_0.setText(String.valueOf(nms.get(8)));
+        this.label2_1.setText(String.valueOf(nms.get(9)));
+        this.label2_2.setText(String.valueOf(nms.get(10)));
+        this.label2_3.setText(String.valueOf(nms.get(11)));
+        this.label3_0.setText(String.valueOf(nms.get(12)));
+        this.label3_1.setText(String.valueOf(nms.get(13)));
+        this.label3_2.setText(String.valueOf(nms.get(14)));
+        this.label3_3.setText(String.valueOf(nms.get(15)));
+        this.label4_0.setText(String.valueOf(nms.get(16)));
+        this.label4_1.setText(String.valueOf(nms.get(17)));
+        this.label4_2.setText(String.valueOf(nms.get(18)));
+        this.label4_3.setText(String.valueOf(nms.get(19)));
+
+        int numaleatorio = (int) (Math.random()*20);
+        this.numero.setText(String.valueOf(nms.get(numaleatorio)));
+        
+
 
     }
 
@@ -110,11 +138,35 @@ public class JuegoController implements Initializable {
 
     @FXML
     public void salir(ActionEvent event) {
-        App.changeRootFXML("vista/Menu");
+        App.changeRootFXML("vista/secundarias/RegistrarAtencion");
     }
+
 
     @FXML
     public void accionJugador(){
-
+        
     }
+
+
+    //Generar numeros aleatorios no repetidos
+    public static ArrayList<Integer> generarNumeros(){
+        ArrayList<Integer> nums = new ArrayList<>();
+        int contador = 0;
+
+        while (contador <20){
+            int rd = (int) (Math.random()*30) +1;
+            Integer i = (int) rd;
+            if(!nums.contains(i)){
+                nums.add(i);
+            }
+            else{ nums.add(contador+31);}
+
+            contador ++;
+                
+            }
+
+
+        return nums;
+    }
+
 }
