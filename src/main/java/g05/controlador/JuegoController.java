54a2081274fe3d11b63d7fe367c6c12f973becfb
AngelTomalaxd <aatomala@espol.edu.ec>
2022-08-19 20:00:44 -0500

    Agregar sonidos en la actividad

--- src/main/java/g05/controlador/JuegoController.java
+++ src/main/java/g05/controlador/JuegoController.java
@@ -1,7 +1,9 @@
 package g05.controlador;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
@@ -30,6 +32,8 @@ import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class JuegoController implements Initializable {
@@ -107,30 +111,10 @@ public class JuegoController implements Initializable {
     ArrayList<Label> labels = new ArrayList<>();
 
     
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        this.label0_0.setText(String.valueOf(nms.get(0)));
-        this.label0_1.setText(String.valueOf(nms.get(1)));
-        this.label0_2.setText(String.valueOf(nms.get(2)));
-        this.label0_3.setText(String.valueOf(nms.get(3)));
-        this.label1_0.setText(String.valueOf(nms.get(4)));
-        this.label1_1.setText(String.valueOf(nms.get(5)));
-        this.label1_2.setText(String.valueOf(nms.get(6)));
-        this.label1_3.setText(String.valueOf(nms.get(7)));
-        this.label2_0.setText(String.valueOf(nms.get(8)));
-        this.label2_1.setText(String.valueOf(nms.get(9)));
-        this.label2_2.setText(String.valueOf(nms.get(10)));
-        this.label2_3.setText(String.valueOf(nms.get(11)));
-        this.label3_0.setText(String.valueOf(nms.get(12)));
-        this.label3_1.setText(String.valueOf(nms.get(13)));
-        this.label3_2.setText(String.valueOf(nms.get(14)));
-        this.label3_3.setText(String.valueOf(nms.get(15)));
-        this.label4_0.setText(String.valueOf(nms.get(16)));
-        this.label4_1.setText(String.valueOf(nms.get(17)));
-        this.label4_2.setText(String.valueOf(nms.get(18)));
-        this.label4_3.setText(String.valueOf(nms.get(19)));
-
+        
         labels.add(label0_0);
         labels.add(label0_1);
         labels.add(label0_2);
@@ -152,14 +136,38 @@ public class JuegoController implements Initializable {
         labels.add(label4_2);
         labels.add(label4_3);
 
-        //casillasVacias(labels, nms);
 
-        int numaleatorio = (int) (Math.random()*nms.size());
-        this.numero.setText(String.valueOf(nms.get(numaleatorio)));
-        nms.remove(numaleatorio);
         
+        this.label0_0.setText(String.valueOf(nms.get(0)));
+        this.label0_1.setText(String.valueOf(nms.get(1)));
+        this.label0_2.setText(String.valueOf(nms.get(2)));
+        this.label0_3.setText(String.valueOf(nms.get(3)));
+        this.label1_0.setText(String.valueOf(nms.get(4)));
+        this.label1_1.setText(String.valueOf(nms.get(5)));
+        this.label1_2.setText(String.valueOf(nms.get(6)));
+        this.label1_3.setText(String.valueOf(nms.get(7)));
+        this.label2_0.setText(String.valueOf(nms.get(8)));
+        this.label2_1.setText(String.valueOf(nms.get(9)));
+        this.label2_2.setText(String.valueOf(nms.get(10)));
+        this.label2_3.setText(String.valueOf(nms.get(11)));
+        this.label3_0.setText(String.valueOf(nms.get(12)));
+        this.label3_1.setText(String.valueOf(nms.get(13)));
+        this.label3_2.setText(String.valueOf(nms.get(14)));
+        this.label3_3.setText(String.valueOf(nms.get(15)));
+        this.label4_0.setText(String.valueOf(nms.get(16)));
+        this.label4_1.setText(String.valueOf(nms.get(17)));
+        this.label4_2.setText(String.valueOf(nms.get(18)));
+        this.label4_3.setText(String.valueOf(nms.get(19)));
 
+        
+        reproducirIdle();
+        cambiarNumero();
 
+        casillasVacias(labels, nms);
+        for(Integer i: nms){
+            System.out.println(i);
+        }
+        
     }
 
 
@@ -178,6 +186,8 @@ public class JuegoController implements Initializable {
     @FXML
     public void accionJugador(){
         comprobarAccion(labels);
+        
+        
     }
 
     @FXML
@@ -189,19 +199,15 @@ public class JuegoController implements Initializable {
     //Generar numeros aleatorios no repetidos
     public static ArrayList<Integer> generarNumeros(){
         ArrayList<Integer> nums = new ArrayList<>();
-        int contador = 0;
 
-        while (contador <20){
-            int rd = (int) (Math.random()*30) +1;
+        while (nums.size() < 20){
+            int rd = (int) (Math.random()*30 +1);
             Integer i = (int) rd;
+
             if(!nums.contains(i)){
                 nums.add(i);
             }
-            else{ nums.add(contador+31);}
-
-            contador ++;
-                
-            }
+        }
 
 
         return nums;
@@ -224,13 +230,15 @@ public class JuegoController implements Initializable {
                     l.setText("");
                     Image im = new Image(App.class.getResourceAsStream("vista/img/gato/gifgato.gif"), 200.0,100.0, true, false);
                     l.setGraphic(new ImageView(im));
-                    cambiarNumero(nms);
-                    
+                    reproducirCorrecto();
+                    cambiarNumero();
+
 
                 } else {
                     numErrores += 1;
                     this.errores.setText(String.valueOf(numErrores));
-                }
+                    reproducirError();
+                } 
 
             });
 
@@ -239,11 +247,11 @@ public class JuegoController implements Initializable {
 
 
     //Cambiar Numero
-    public void cambiarNumero(ArrayList<Integer> lb){
+    public void cambiarNumero(){
 
-        int numA = (int) (Math.random()*lb.size());
-        this.numero.setText(String.valueOf(lb.get(numA)));
-        lb.remove(numA);
+        int numA = (int) (Math.random()*nms.size());
+        this.numero.setText(String.valueOf(nms.get(numA)));
+        nms.remove(numA);
 
     }
 
@@ -258,16 +266,13 @@ public class JuegoController implements Initializable {
     //Generar Casillas Vacias
     public void casillasVacias(ArrayList<Label> lbs, ArrayList<Integer> ints){
         ArrayList<Integer> indices = new ArrayList<>();
-        int contador =0;
 
-        while (contador <6){
+        while (indices.size() <5){
             int c = (int) (Math.random()*ints.size());
             Integer n = (int) c;
             if(!indices.contains(n)){
                 indices.add(n);
             }
-            //else { n++; indices.add(n); }
-            contador++;
         }
 
         for (Integer i: indices){
@@ -277,5 +282,24 @@ public class JuegoController implements Initializable {
         }
     }
 
+    public void reproducirCorrecto(){
+        Media correcto = new Media(new File("src/main/resources/g05/vista/sound-fx/sonidosJuego/correcto.mp3").toURI().toString());
+        MediaPlayer correctoP = new MediaPlayer(correcto);
+        correctoP.play();
+    }
+
+    public void reproducirError(){
+        Media error = new Media(new File("src/main/resources/g05/vista/sound-fx/sonidosJuego/error.mp3").toURI().toString());
+        MediaPlayer errorP = new MediaPlayer(error);
+        errorP.play();
+    }
+
+    public void reproducirIdle(){
+        Media soundtrack = new Media(new File("src/main/resources/g05/vista/sound-fx/sonidosJuego/juego.mp3").toURI().toString());
+        MediaPlayer soundtrackP = new MediaPlayer(soundtrack);
+        soundtrackP.setAutoPlay(true);
+        soundtrackP.play();
+    
+    }
 
 }
